0df0390448040e3ae106b7fdacb36ef15ebfd20a
Dejan <dejannrt411@gmail.com>
2016-12-19 01:32:01 +0100

    Creating a Movie model and migration for movies table

---
+++ database/migrations/2016_12_18_235851_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('movie');
+            $table->integer('year');
+            $table->string('director');
+            $table->text('synopsis');
+            $table->string('youtube_trailer');
+            $table->float('imdb_rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
