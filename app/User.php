e3eddfd937978fc067804314b129b1561ecdf8b2
Dejan <dejannrt411@gmail.com>
2016-12-20 15:38:00 +0100

    Adding relations to a User model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //One user has many comments
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
