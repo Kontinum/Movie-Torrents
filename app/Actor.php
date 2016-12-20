da88e81a411dc41dcdb67a9a53099e9d84fb4586
Dejan <dejannrt411@gmail.com>
2016-12-20 15:27:34 +0100

    Adding realtions to an Actor model

--- app/Actor.php
+++ app/Actor.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Actor extends Model
 {
-    //
+    protected $table = "actors";
+
+    //One actor belongs to many movies
+    public function movies()
+    {
+        return $this->belongsToMany(Movie::class);
+    }
 }
