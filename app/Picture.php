64fd33f2208d86593b57b04f3022dffbcdf7faf8
Dejan <dejannrt411@gmail.com>
2016-12-20 15:35:31 +0100

    Adding relations to the Picture model

--- app/Picture.php
+++ app/Picture.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Picture extends Model
 {
-    //
+    protected $table = "pictures";
+
+    //One picture belongs to a movie
+    public function movies()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
