806fa2b090e6364aa36085fcd28b26d9ae65febb
Dejan <dejannrt411@gmail.com>
2016-12-20 15:25:03 +0100

    Adding relations to a Movie model

--- app/Movie.php
+++ app/Movie.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    protected $table = "movies";
+
+    //One movie has one torrent
+    public function torrent()
+    {
+        return $this->hasOne(Torrent::class);
+    }
+
+    //One movie has many pictures
+    public function pictures()
+    {
+        return $this->hasMany(Picture::class);
+    }
+
+    //One movie belongs to many genres
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class);
+    }
+
+    //One movie has many comments
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    //One movie belongs to many actors
+    public function actors()
+    {
+        return $this->belongsToMany(Actor::class);
+    }
 }
