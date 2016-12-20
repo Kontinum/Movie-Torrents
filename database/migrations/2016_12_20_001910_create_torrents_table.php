5d9f3f974b404728d246515db0744e2c608319aa
Dejan <dejannrt411@gmail.com>
2016-12-20 01:26:07 +0100

    Creating the Torrent model and migration for torrents table

---
+++ database/migrations/2016_12_20_001910_create_torrents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTorrentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('torrents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('movie_id')->unsigned();
+            $table->string('size');
+            $table->string('resolution');
+            $table->string('audio');
+            $table->string('length');
+            $table->string('fps');
+            $table->string('p/s');
+            $table->string('pg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('torrents');
+    }
+}
