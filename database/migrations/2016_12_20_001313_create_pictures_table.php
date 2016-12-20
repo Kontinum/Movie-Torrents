16ef4188bd9b81b2710db7097ffaaf184942712d
Dejan <dejannrt411@gmail.com>
2016-12-20 01:17:18 +0100

    Creating a picture model and migration for pictures table

---
+++ database/migrations/2016_12_20_001313_create_pictures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('movie_id')->unsigned();
+            $table->string('picture1_path');
+            $table->string('picture2_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
