f519fd03f2e32c38916b88baf9091f6029a13d0c
Dejan <dejannrt411@gmail.com>
2016-12-20 15:32:48 +0100

    Adding relations to the Torrent model

--- app/Torrent.php
+++ app/Torrent.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Torrent extends Model
 {
-    //
+    protected $table = "torrents";
+
+    //Torrent belongs to a movie
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
