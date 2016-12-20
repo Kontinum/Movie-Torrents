5c7dbff6983cef49ac62abcfebce082eb91d4e46
Dejan <dejannrt411@gmail.com>
2016-12-20 15:30:09 +0100

    Adding relations to Genre model

--- app/Genre.php
+++ app/Genre.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-    //
+    protected $table = "genres";
+
+    //One genre belongs to many movies
+    public function movies()
+    {
+        return $this->belongsToMany(Movie::class);
+    }
 }
