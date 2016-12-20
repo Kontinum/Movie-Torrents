e74e18d1b10c3a853b6e4c5eaf4c61688af525ae
Dejan <dejannrt411@gmail.com>
2016-12-20 15:41:02 +0100

    Adding relations to the Comment model

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = "comments";
+
+    //One comment belongs to a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //One comment belongs to a movie
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
