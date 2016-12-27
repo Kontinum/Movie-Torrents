b1732ad45da792289806ff3cd23de850d15eff59
Dejan <dejannrt411@gmail.com>
2016-12-27 23:44:02 +0100

    Changing name of route link to postGenre

--- resources/views/genres.blade.php
+++ resources/views/genres.blade.php
@@ -7,7 +7,7 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Add Genre</div>
                     <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ route('postGenres') }}">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('postGenre') }}">
                             {{ csrf_field() }}
 
                             <div class="form-group{{ $errors->has('genre') ? ' has-error' : '' }}">
