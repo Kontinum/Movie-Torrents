86d9d298be32fca09cd32347689941223ccab1be
Dejan <dejannrt411@gmail.com>
2016-12-27 17:48:48 +0100

    Changing footer markup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,15 +83,13 @@
         @yield('content')
     </div>
 
-        <div class="container-fluid">
-            <div class="row">
+    <div class="container-fluid">
+        <div class="row">
             <div class="footer text-center">
                 <p>&copy; MovieTorrents {{date('Y')}}.</p>
             </div>
-            </div>
-            </div>
-
-
+        </div>
+    </div>
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
