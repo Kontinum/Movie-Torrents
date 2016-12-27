fec2a4059f68c5d7640c1a7f48bfa76c3e0add4d
Dejan <dejannrt411@gmail.com>
2016-12-27 23:03:38 +0100

    Adding redirection if user is not admin in getGenres method and changing eloquent query

--- app/Http/Controllers/AdminPagesController.php
+++ app/Http/Controllers/AdminPagesController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminPagesController extends Controller
 {
     //Return all genres order by name
     public function getGenres()
     {
-        $genres = Genre::all()->orderBy('name','ASC')->get();
+        foreach(Auth::user()->roles as $role) {
+            if ($role->name !== 'admin') {
+                return redirect()->route('home');
+            }
+        }
+        $genres = Genre::orderBy('name','ASC')->get();
 
         return view('genres')->with('genres',$genres);
     }
