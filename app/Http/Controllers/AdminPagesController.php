c3dbe780c8b0d60462e617952241d60776f856f5
Dejan <dejannrt411@gmail.com>
2016-12-27 21:37:05 +0100

    Adding getGenres method

--- app/Http/Controllers/AdminPagesController.php
+++ app/Http/Controllers/AdminPagesController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Genre;
 use Illuminate\Http\Request;
 
 class AdminPagesController extends Controller
 {
-    //
+    //Return all genres order by name
+    public function getGenres()
+    {
+        $genres = Genre::all()->orderBy('name','ASC')->get();
+
+        return view('genres')->with('genres',$genres);
+    }
 }
