7f2a00cdb4781f5541543cf4e7adaaa8494238f4
Dejan <dejannrt411@gmail.com>
2016-12-29 19:02:43 +0100

    Editing class name for genre-edit drop down menu

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,15 @@
 $('document').ready(function(){
    $('.genre-edit').hide();
 
-    var brojZanrova = $('.list-group-item').length;
+    var brojZanrova = $('.genre-list').length;
     var nizZanrova = [];
     for (brojZanrova;brojZanrova>0;brojZanrova--){
         nizZanrova.push(brojZanrova);
     }
 
     $.each(nizZanrova,function(index , value){
-    $('.list-group-item:nth-child('+ value +') .fa-pencil-square-o').click(function(){
-            $('.list-group-item:nth-child('+ value +') .genre-edit').slideToggle()
+    $('.genre-list:nth-child('+ value +') .fa-pencil-square-o').click(function(){
+            $('.genre-list:nth-child('+ value +') .genre-edit').slideToggle()
         });
     })
 });
