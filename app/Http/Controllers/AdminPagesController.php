a90d0aa9feddc4efc6c17e89c90f94143335724d
Dejan <dejannrt411@gmail.com>
2016-12-29 23:16:50 +0100

    Adding postActor method

--- app/Http/Controllers/AdminPagesController.php
+++ app/Http/Controllers/AdminPagesController.php
@@ -17,6 +17,38 @@ class AdminPagesController extends Controller
         return view('actors')->with('actors',$actors);
     }
 
+    //Add new actor
+    public function postActor(Request $request)
+    {
+        $this->validate($request,[
+            'actor'=>'required|min:5|max:30|regex:/^[(a-zA-Z\s)]+$/u',
+            'birth_year'=>'required|numeric',
+            'actor_image'=>'dimensions:max_width=256,max_height=256|mimes:jpg,jpeg,png'
+        ]);
+
+        $actor = Actor::where('name',$request['actor'])->where('birth_year',$request['birth_year'])->first();
+
+        if($actor){
+            return redirect()->route('getActors')->with(['fail'=>'Actor '.$actor->name.' is already in database']);
+        }
+
+        $actor = new Actor();
+        $actor->name = $request['actor'];
+        $actor->birth_year = $request['birth_year'];
+
+        if($request->hasFile('actor_image')){
+            $actor_image = $request->file('actor_image');
+            $filename = time(). '.'. $actor_image->getClientOriginalName();
+            $actor_image->move('images/actors/',$filename);
+
+            $actor->thumbnail_path = $filename;
+        }
+
+        $actor->save();
+
+        return redirect()->route('getActors')->with(['success'=>'Actor '.$request['actor'].' successfully added']);
+    }
+
     //Return all genres order by name
     public function getGenres()
     {
