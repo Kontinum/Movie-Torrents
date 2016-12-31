0eb16bf134962790abce28075a095803996d51cd
Dejan <dejannrt411@gmail.com>
2016-12-31 13:38:03 +0100

    Changing way to determine who si admin in order to show drop down

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
                                 <ul class="dropdown-menu" role="menu">
                                     <li><a href=""><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
                                     @foreach(Auth::user()->roles as $role)
-                                        @if($role->name == 'admin')
+                                        @if($role)
                                             <li><a href="#"><i class="fa fa-users" aria-hidden="true"></i> Users</a></li>
                                             <li><a href="#"><i class="fa fa-film" aria-hidden="true"></i> Movies</a></li>
                                             <li><a href="{{route('getActors')}}"><i class="fa fa-user-circle" aria-hidden="true"></i> Actors</a></li>
