b9610332daf6b7424a03530baaed4d4e2e1e7c09
Dejan <dejannrt411@gmail.com>
2017-01-02 17:08:32 +0100

    Adding form to search users

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -72,4 +72,40 @@
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Search users</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="GET" action="{{route('getSearchUsers')}}">
+
+
+                            <div class="form-group{{ $errors->has('actor_name') ? ' has-error' : '' }}">
+                                <label for="user_name" class="col-md-4 control-label">User name:</label>
+
+                                <div class="col-md-6">
+                                    <input id="user_name" type="text" class="form-control" name="user_name" value="{{ old('user_name') }}" required autofocus>
+
+                                    @if ($errors->has('user_name'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('actor_name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Search users
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
