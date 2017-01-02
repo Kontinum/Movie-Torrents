3e48b0194dc5d04c8ce41b149baec468a3767fa7
Dejan <dejannrt411@gmail.com>
2017-01-02 16:58:09 +0100

    Editing submit button value in form for user creating

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -62,7 +62,7 @@
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Register
+                                        Add new user
                                     </button>
                                 </div>
                             </div>
