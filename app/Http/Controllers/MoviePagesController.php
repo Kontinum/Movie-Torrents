d83da3a329229e5a13f1ecd7c6da2ed1fd5f6bf6
Dejan <dejannrt411@gmail.com>
2017-01-04 13:09:18 +0100

    Editing wrong added model in eloquent query

--- app/Http/Controllers/MoviePagesController.php
+++ app/Http/Controllers/MoviePagesController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Actor;
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MoviePagesController extends Controller
 {
     public function getMovies()
     {
-        $count_movies = Actor::all()->count();
+        $count_movies = Movie::all()->count();
 
         return view('movies')->with('count_movies',$count_movies);
     }
