d0cd492435d1c235417982f09212d6850075b69c
Dejan <dejannrt411@gmail.com>
2017-01-04 18:58:18 +0100

    Adding wrapper around container in order to stop content to go under the footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,76 +23,77 @@
     </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+    <div class="wrapper">
+        <div id="app">
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                        <!-- Collapsed Hamburger -->
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        mOvIeToRrEnTs
-                    </a>
-                </div>
+                        <!-- Branding Image -->
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            mOvIeToRrEnTs
+                        </a>
+                    </div>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+                    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="nav navbar-nav">
+                            &nbsp;
+                        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    <img style="width:40px;height:40px;border-radius: 50%" src="/images/users/{{Auth::user()->profile_picture}}" alt="">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="nav navbar-nav navbar-right">
+                            <!-- Authentication Links -->
+                            @if (Auth::guest())
+                                <li><a href="{{ url('/login') }}">Login</a></li>
+                                <li><a href="{{ url('/register') }}">Register</a></li>
+                            @else
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                        <img style="width:40px;height:40px;border-radius: 50%" src="/images/users/{{Auth::user()->profile_picture}}" alt="">
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{route('getProfile',['user_id'=>Auth::id()])}}"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
-                                    @foreach(Auth::user()->roles as $role)
-                                        @if($role)
-                                            <li><a href="{{route('getUsers')}}"><i class="fa fa-users" aria-hidden="true"></i> Users</a></li>
-                                            <li><a href="{{route('getMovies')}}"><i class="fa fa-film" aria-hidden="true"></i> Movies</a></li>
-                                            <li><a href="{{route('getActors')}}"><i class="fa fa-user-circle" aria-hidden="true"></i> Actors</a></li>
-                                            <li><a href="{{route('getGenres')}}"><i class="fa fa-video-camera" aria-hidden="true"></i> Genres</a></li>
-                                        @endif
-                                    @endforeach
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="{{route('getProfile',['user_id'=>Auth::id()])}}"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
+                                        @foreach(Auth::user()->roles as $role)
+                                            @if($role)
+                                                <li><a href="{{route('getUsers')}}"><i class="fa fa-users" aria-hidden="true"></i> Users</a></li>
+                                                <li><a href="{{route('getMovies')}}"><i class="fa fa-film" aria-hidden="true"></i> Movies</a></li>
+                                                <li><a href="{{route('getActors')}}"><i class="fa fa-user-circle" aria-hidden="true"></i> Actors</a></li>
+                                                <li><a href="{{route('getGenres')}}"><i class="fa fa-video-camera" aria-hidden="true"></i> Genres</a></li>
+                                            @endif
+                                        @endforeach
+                                        <li>
+                                            <a href="{{ url('/logout') }}"
+                                               onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            <i class="fa fa-lg fa-sign-out" aria-hidden="true"></i>Logout
-                                        </a>
+                                                <i class="fa fa-lg fa-sign-out" aria-hidden="true"></i>Logout
+                                            </a>
 
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
+                                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </li>
+                            @endif
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-        @include('/errors/errorsinfo')
-        @yield('content')
+            </nav>
+            @include('/errors/errorsinfo')
+            @yield('content')
+        </div>
     </div>
-
 
     <div class="container-fluid">
         <div class="row">
