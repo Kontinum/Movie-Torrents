5c9cc7b5734a32754eff7dc4332192ee278d91b5
Dejan <dejannrt411@gmail.com>
2017-01-04 20:30:15 +0100

    Adding eloquent queries to get all actors and genres in getMovies method

--- app/Http/Controllers/MoviePagesController.php
+++ app/Http/Controllers/MoviePagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Actor;
+use App\Genre;
 use App\Movie;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,9 @@ class MoviePagesController extends Controller
     public function getMovies()
     {
         $count_movies = Movie::all()->count();
+        $genres = Genre::all();
+        $actors = Actor::all();
 
-        return view('movies')->with('count_movies',$count_movies);
+        return view('movies')->with('count_movies',$count_movies)->with('genres',$genres)->with('actors',$actors);
     }
 }
