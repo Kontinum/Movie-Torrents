3ddeca23afe860893ee22b81532da6fdcd2f9861
Dejan <dejannrt411@gmail.com>
2017-01-08 14:47:07 +0100

    Adding code to handle drop down movie form

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,7 +13,21 @@ $('document').ready(function(){
         });
     })
 
+    //Delay for session messages
     $('.info-box.success').delay(4000).slideUp(1000);
     $('.info-box.fail').delay(4000).slideUp(1000);
+
+    //Drop down for add-movie-form
+    $('.add-movie-form').hide();
+    $('.dropdown-movie').click(function () {
+        $('.add-movie-form').slideToggle();
+        if($('.dropdown-movie-icon').hasClass('fa-chevron-down')){
+            $('.dropdown-movie-icon').removeClass('fa-chevron-down');
+            $('.dropdown-movie-icon').addClass('fa-chevron-up');
+        } else{
+            $('.dropdown-movie-icon').removeClass('fa-chevron-up');
+            $('.dropdown-movie-icon').addClass('fa-chevron-down');
+        }
+    });
 
 });
