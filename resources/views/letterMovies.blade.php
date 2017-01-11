67a71216342328734ee2b216e22c1a6608efda2a
Dejan <dejannrt411@gmail.com>
2017-01-11 23:01:54 +0100

    Adding links for editMovie route

--- resources/views/letterMovies.blade.php
+++ resources/views/letterMovies.blade.php
@@ -13,7 +13,7 @@
                     <li class="list-group-item col-md-3 col-sm-4 col-xs-6 text-center">
                         {{$movie->name}} - {{$movie->year}}
                         <img class="img-responsive" src="{{'/images/movies/'.$movie->name.'-'.$movie->year.'/'.$movie->pictures->poster_picture}}" alt="{{$movie->name}} poster image">
-                        <a href="" title="Edit movie"><i style="line-height:20px;" class="fa fa-lg fa-pencil-square-o list-icons" aria-hidden="true"></i></a>
+                        <a href="{{route('getEditMovie',['movie_id'=>$movie->id])}}" title="Edit movie"><i style="line-height:20px;" class="fa fa-lg fa-pencil-square-o list-icons" aria-hidden="true"></i></a>
                         <a style="line-height: 45px" href="{{route('deleteMovie',['movie_id'=>$movie->id])}}" title="Delete movie"><i class="fa fa-lg fa-trash list-icons" aria-hidden="true"></i></a>
                     </li>
                 @endforeach
