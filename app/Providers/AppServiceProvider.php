81f6304c7d1bfb227c2c1fddd0872a7259925bf2
Dejan <dejannrt411@gmail.com>
2017-01-11 01:10:06 +0100

    Adding custom validation for youtube embed code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('youtube',function($attribute, $value, $parameters){
+            if(strpos('x'.$value,'https://www.youtube.com/embed/') == '1'){
+                return true;
+            }else{
+                return false;
+            }
+        });
     }
 
     /**
