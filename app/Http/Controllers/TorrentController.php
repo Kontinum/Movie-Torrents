d797d975f46e172d5ce27d7053f3f3e7cfaf4c44
Dejan <dejannrt411@gmail.com>
2017-01-13 20:06:03 +0100

    Creating TorrentController and torrentDownload method

--- app/Http/Controllers/TorrentController.php
+++ app/Http/Controllers/TorrentController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class TorrentController extends Controller
 {
-    //
+    public function downloadTorrent()
+    {
+        return response()->download(public_path().'/torrents/movie.torrent');
+    }
 }
