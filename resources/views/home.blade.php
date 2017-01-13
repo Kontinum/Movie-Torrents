570682e638afba9f267bfe6c3da57c33a2a8c9ba
Dejan <dejannrt411@gmail.com>
2017-01-13 19:26:19 +0100

    Outputting popular and new movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,28 +15,37 @@
                 <hr>
             </div>
             <div class="row">
-                <div class="col-lg-3 col-lg-offset-0 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-12 col-xs-offset-0 ">
-                    <div class="movie">
-                        <a href="#" class="movie-link">
-                            <figure>
-                                <img class="img-responsive image-list" src="/images/ipman.jpg" alt="ipman">
-                                <figcaption class="hidden-sm hidden-xs">
-                                    <br><br><br><br>
-                                    <h4 class="rating">7 / 10</h4>
-                                    <h4>Action</h4>
-                                    <h4>Biography</h4>
-                                </figcaption>
-                            </figure>
-                        </a>
-                        <div class="movie-bottom">
-                            <a href="#" class="movie-link-name">Ip Man</a>
-                            <div class="movie-year">2010</div>
-                            <a href="#" class="movie-link-download">
-                                <i class="fa fa-2x fa-download"></i>
+                @foreach($popular_movies as $movie)
+                    <div class="col-lg-3 col-lg-offset-0 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-12 col-xs-offset-0 ">
+                        <div class="movie">
+                            <a href="#" class="movie-link">
+                                <figure>
+                                    <img class="img-responsive image-list" src="/images/movies/{{$movie->name .'-'.$movie->year .'/' .$movie->pictures->poster_picture}}" alt="ipman">
+                                    <figcaption class="hidden-sm hidden-xs">
+                                        <br><br><br><br><br>
+                                        <h4 class="rating">{{$movie->imdb_rating}}/10</h4>
+                                        @foreach($movie->genres()->limit(2)->get() as $genre)
+                                            <h4>{{$genre->name}}</h4>
+                                        @endforeach
+                                    </figcaption>
+                                </figure>
                             </a>
+                            <div class="movie-bottom">
+                                <a href="#" class="movie-link-name">
+                                    @if(strlen($movie->name) > 18)
+                                        {{substr($movie->name,0,25).'...'}}
+                                        @else
+                                        {{$movie->name}}
+                                    @endif
+                                </a>
+                                <div class="movie-year">{{$movie->year}}</div>
+                                <a href="#" class="movie-link-download">
+                                    <i class="fa fa-2x fa-download"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </section>
@@ -47,28 +56,37 @@
                 <hr>
             </div>
             <div class="row">
-                <div class="col-lg-3 col-lg-offset-0 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-12 col-xs-offset-0 ">
-                    <div class="movie">
-                        <a href="#" class="movie-link">
-                            <figure>
-                                <img class="img-responsive image-list" src="/images/ipman.jpg" alt="ipman">
-                                <figcaption class="hidden-sm hidden-xs">
-                                    <br><br><br><br>
-                                    <h4 class="rating">7 / 10</h4>
-                                    <h4>Action</h4>
-                                    <h4>Biography</h4>
-                                </figcaption>
-                            </figure>
-                        </a>
-                        <div class="movie-bottom">
-                            <a href="#" class="movie-link-name">Ip Man</a>
-                            <div class="movie-year">2010</div>
-                            <a href="#" class="movie-link-download">
-                                <i class="fa fa-2x fa-download"></i>
+                @foreach($new_movies as $movie)
+                    <div class="col-lg-3 col-lg-offset-0 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1 col-xs-12 col-xs-offset-0 ">
+                        <div class="movie">
+                            <a href="#" class="movie-link">
+                                <figure>
+                                    <img class="img-responsive image-list" src="/images/movies/{{$movie->name .'-'.$movie->year .'/' .$movie->pictures->poster_picture}}" alt="ipman">
+                                    <figcaption class="hidden-sm hidden-xs">
+                                        <br><br><br><br><br>
+                                        <h4 class="rating">{{$movie->imdb_rating}}/10</h4>
+                                        @foreach($movie->genres()->limit(2)->get() as $genre)
+                                            <h4>{{$genre->name}}</h4>
+                                        @endforeach
+                                    </figcaption>
+                                </figure>
                             </a>
+                            <div class="movie-bottom">
+                                <a href="#" class="movie-link-name">
+                                    @if(strlen($movie->name) > 18)
+                                        {{substr($movie->name,0,25).'...'}}
+                                    @else
+                                        {{$movie->name}}
+                                    @endif
+                                </a>
+                                <div class="movie-year">{{$movie->year}}</div>
+                                <a href="#" class="movie-link-download">
+                                    <i class="fa fa-2x fa-download"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </section>
