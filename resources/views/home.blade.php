24e10b34e35c37e4369573d0ac696cd1d3f1648c
Dejan <dejannrt411@gmail.com>
2017-01-13 20:04:29 +0100

    Adding route link for torrent download

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,7 +39,7 @@
                                     @endif
                                 </a>
                                 <div class="movie-year">{{$movie->year}}</div>
-                                <a href="#" class="movie-link-download">
+                                <a href="{{route('downloadTorrent')}}" class="movie-link-download">
                                     <i class="fa fa-2x fa-download"></i>
                                 </a>
                             </div>
@@ -80,7 +80,7 @@
                                     @endif
                                 </a>
                                 <div class="movie-year">{{$movie->year}}</div>
-                                <a href="#" class="movie-link-download">
+                                <a href="{{route('downloadTorrent')}}" class="movie-link-download">
                                     <i class="fa fa-2x fa-download"></i>
                                 </a>
                             </div>
