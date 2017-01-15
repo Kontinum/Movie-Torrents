c545983f79f0d20c8181e4812e8cab80286a34dd
Dejan <dejannrt411@gmail.com>
2017-01-15 18:24:41 +0100

    Adding lightbox.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
     <link href="/css/app.css" rel="stylesheet">
     <!-- Select2 css -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
-
+    <!-- Lightbox css -->
+    <link rel="stylesheet" href="/css/lightbox.min.css">
     <link rel="stylesheet" href="/css/styles.css">
     <link rel="stylesheet" href="/css/errors.css">
 
@@ -112,6 +113,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <!-- Select2 js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+    <!-- lightbox -->
+    <script src="/js/lightbox.min.js"></script>
     <script src="/js/scripts.js"></script>
     @yield('script')
 </body>
