56dc1f60201650f8a5bcd77aeff7e81d172b49ee
Dejan <dejannrt411@gmail.com>
2017-01-16 15:39:39 +0100

    Creating accessor for created_at column and changing date format with Carbon

--- app/Movie.php
+++ app/Movie.php
@@ -2,11 +2,19 @@
 
 namespace App;
 
+use Carbon\Carbon;
+use Faker\Provider\cs_CZ\DateTime;
 use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
     protected $table = "movies";
+
+    public function getCreatedAtAttribute($value)
+    {
+       $date = Carbon::parse($value);
+       return $date->format('d-m-Y');
+    }
 
     //One movie has one torrent
     public function torrent()
