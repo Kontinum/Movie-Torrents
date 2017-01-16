7ee0ff2787a061458cd43d01ddf3609e8c3d8b29
Dejan <dejannrt411@gmail.com>
2017-01-16 14:36:40 +0100

    Adding logic to increase movie download number in torrentDownload method

--- app/Http/Controllers/TorrentController.php
+++ app/Http/Controllers/TorrentController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class TorrentController extends Controller
 {
-    public function downloadTorrent()
+    public function downloadTorrent($movie_id)
     {
+        $movie = Movie::find($movie_id);
+        $movie->downloaded += 1;
+        $movie->save();
         return response()->download(public_path().'/torrents/movie.torrent');
     }
 }
