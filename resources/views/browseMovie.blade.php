3985fbc0d734365237f23c9c4983be04abad2001
Dejan <dejannrt411@gmail.com>
2017-01-17 13:03:45 +0100

    Displaying all comments and form to add a comment

--- resources/views/browseMovie.blade.php
+++ resources/views/browseMovie.blade.php
@@ -87,14 +87,41 @@
     <div class="container">
         <div class="row">
             <h3>Comments:</h3>
+            <div class="browse-movie-comments">
+                @if(count($comments) == 0)
+                    <p>There are no comments for this movie. Add one!</p>
+                @else
+                    @foreach($comments as $comment)
+                        <div class="browse-movie-comment col-lg-12">
+                            <div class="col-lg-4">{{$comment->user->name}}
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="col-lg-1">
+                                    <img width="50px" height="50px" style="border-radius: 50%" src="/images/users/{{$comment->user->profile_picture}}" alt="">
+                                </div>
+                                <div class="col-lg-8">
+                                    <strong>{{$comment->text}}</strong>
+                                    @if($comment->user_id == Auth::id())
+                                        <a title="Delete your comment" href="{{route('deleteComment',['comment_id'=>$comment->id])}}"><i class="pull-right fa fa-lg fa-times" aria-hidden="true"></i></a>
+                                    @endif
+                                    <br> <i>{{$comment->created_at}}</i>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
             @if(!Auth::check())
                 Please <a href="{{route('login')}}">login</a> in order to post a comment.
             @else
-                <div class="col-lg-12">
-                    <form class="form-horizontal" action="" method="post">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <form class="form-horizontal" action="{{route('postComment')}}" method="post">
+                        {{csrf_field()}}
+                        <input type="hidden" name="movie_id" value="{{$movie->id}}">
+                        <input type="hidden" name="user_id" value="{{Auth::id()}}">
                         <div class="form-group">
-                            <label class="control-label" for="movie_comment">Post a comment</label>
-                            <textarea class="form-control" name="movie_comment" id="movie_comment" cols="30" rows="5"></textarea>
+                            <label class="control-label" for="comment_body">Post a comment (max 400)</label>
+                            <textarea class="form-control" name="comment_body" id="comment_body" cols="30" rows="5"></textarea>
                         </div>
                         <div class="form-group">
                             <button class="btn btn-primary" type="submit">Post a comment</button>
