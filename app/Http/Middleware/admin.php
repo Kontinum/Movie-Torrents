5194a593acd8caf2a294f03a18c602e347d20783
Dejan <dejannrt411@gmail.com>
2017-01-24 23:15:46 +0100

    Creating admin middleware

---
+++ app/Http/Middleware/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $role = Auth::user()->roles()->get();
+        if ($role->isEmpty()) {
+            return redirect()->route('home');
+        }
+        return $next($request);
+    }
+}
