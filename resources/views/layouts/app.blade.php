1cb4546bc8b421eceac8a39c24a2f183954a7eae
Dejan <dejannrt411@gmail.com>
2017-01-28 12:45:38 +0100

    Adding check to see if user is admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,7 @@
                                     <ul class="dropdown-menu" role="menu">
                                         <li><a href="{{route('getProfile',['user_id'=>Auth::id()])}}"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
                                         @foreach(Auth::user()->roles as $role)
-                                            @if($role)
+                                            @if($role->name == 'admin')
                                                 <li><a href="{{route('getUsers')}}"><i class="fa fa-users" aria-hidden="true"></i> Users</a></li>
                                                 <li><a href="{{route('getMovies')}}"><i class="fa fa-film" aria-hidden="true"></i> Movies</a></li>
                                                 <li><a href="{{route('getActors')}}"><i class="fa fa-user-circle" aria-hidden="true"></i> Actors</a></li>
