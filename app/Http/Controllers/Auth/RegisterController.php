877b5a4da8faaca8b765d6282635ff8e653759c6
Dejan <dejannrt411@gmail.com>
2017-01-28 12:44:11 +0100

    Editing RegisterController to attach role

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -62,10 +63,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+        $current_user = User::find(DB::getPdo()->lastInsertId());
+
+        $current_user->roles()->attach(2);
+
+        return $user;
     }
 }
